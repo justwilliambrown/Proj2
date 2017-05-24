47bb8cf3be10e04e8fbf141b9abb39ab9de7a3fe
William <justwilliambrown101@gmail.com>
2017-05-25 04:16:35 +0800

    Updated ngramanalyser.java

--- NgramAnalyser.java
+++ NgramAnalyser.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.TreeMap;
 
 import java.util.HashSet;
 import java.util.Arrays;
@@ -20,144 +21,150 @@ import java.util.Arrays;
  */
 public class NgramAnalyser
 {
-    /** dictionary of all distinct n-grams and their frequencies */
-    private HashMap<String,Integer> ngram;
+	/** dictionary of all distinct n-grams and their frequencies */
+	private HashMap<String,Integer> ngram;
 
-    /** number of distinct characters in the input */
-    private int alphabetSize;
+	/** number of distinct characters in the input */
+	private int alphabetSize;
 
-    /** n-gram size for this object (new field) */
-    private int ngramSize;
+	/** n-gram size for this object (new field) */
+	private int ngramSize;
 
-    /** 
-     * Analyse the frequency with which distinct n-grams, of length n,
-     * appear in an input string. 
-     * n-grams at the end of the string wrap to the front
-     * e.g. "abbbbc" includes "bca" and "cab" in its 3-grams
-     * @param int n size of n-grams to create
-     * @param String inp input string to be modelled
-     */
-    public NgramAnalyser(int n, String inp) 
-    { 
-        //TODO replace this line with your code || No fuck off
-    	
-    	// Check for validity
-    	if (inp == "" || inp == null || n == 0 || inp.length() < n)
-    	{
-    		throw new IllegalArgumentException("Invalid Input");
-    	}
-    	
-    	int cur_pos = 0;
-    	// Splice the n characters (including cur_pos)
-    	// Check if they match any ngrams currently in the hashmap
-    	// If there are: Increase frequency by 1
-    	// If there aren't: Add it with a frequency of 1
-    	for (cur_pos = 0; cur_pos <= (inp.length() + 1); cur_pos++)
-    	{
-    		String thisNgram = "";
-    		if (cur_pos + n < inp.length()){
-    			thisNgram = inp.substring(cur_pos, (cur_pos+n));
-    		}
-    		else
-    		{
-    			String fromEnd = inp.substring(cur_pos, (inp.length() + 1));
-    			String fromStart = inp.substring(0, (n - fromEnd.length() + 1));
-    			thisNgram = fromEnd + fromStart;
-    		}
-    		
-    		if (ngram.containsKey(thisNgram))
-    		{
-    			ngram.put(thisNgram, (ngram.get(thisNgram) + 1));
-    		} else
-    		{
-    			ngram.put(thisNgram, 1);
-    		}
-    	}
-    	
-    	// Deal with the wraparound
-    	int fromEnd = (inp.length() % n);
-    	int fromStart = (n - fromEnd);
-    	String wrapNgram = inp.substring(inp.length() - fromEnd, (inp.length() + 1)) + inp.substring(0, fromStart + 1);
-    	if (ngram.containsKey(wrapNgram))
+	/** 
+	 * Analyse the frequency with which distinct n-grams, of length n,
+	 * appear in an input string. 
+	 * n-grams at the end of the string wrap to the front
+	 * e.g. "abbbbc" includes "bca" and "cab" in its 3-grams
+	 * @param int n size of n-grams to create
+	 * @param String inp input string to be modelled
+	 */
+	private int stringSize = 0;
+	
+	public NgramAnalyser(int n, String inp) 
+	{     	
+		ngram = new HashMap<String, Integer>();
+		
+		// Check for validity
+		if (inp == "" || inp == null || n == 0 || inp.length() < n)
 		{
-			ngram.put(wrapNgram, (ngram.get(wrapNgram) + 1));
-		} else
-		{
-			ngram.put(wrapNgram, 1);
+			throw new IllegalArgumentException("Invalid Input");
 		}
-    	
-    	
-    	// TODO Count number of unique characters
-    	// Fucked if I know how to do that.
-    	
-    }
+		
+		// Do the string size thing
+		stringSize = inp.length();
+		
+		int cur_pos = 0;
+		// Splice the n characters (including cur_pos)
+		// Check if they match any ngrams currently in the hashmap
+		// If there are: Increase frequency by 1
+		// If there aren't: Add it with a frequency of 1
+		for (cur_pos = 0; cur_pos < (inp.length()); cur_pos++)
+		{	
+			String thisNgram = "";
+			if (cur_pos + n <= inp.length())
+			{
+				thisNgram = inp.substring(cur_pos, (cur_pos+n));
+			}
+			else
+			{
+				String fromEnd = inp.substring(cur_pos, (inp.length()));
+				String fromStart = inp.substring(0, (n - fromEnd.length()));
+				thisNgram = fromEnd + fromStart;
+			}
 
-    /** 
-     * Analyses the input text for n-grams of size 1.
-     */
-    public NgramAnalyser(String inp) 
-    {
-        this(1,inp);
-    }
+			if (ngram.containsKey(thisNgram))
+			{
+				ngram.put(thisNgram, (ngram.get(thisNgram) + 1));
+			} else
+			{
+				ngram.put(thisNgram, 1);
+				ngramSize++;
+			}
+		}
+		
+		// TODO Count number of unique characters
+		if(n == 1)
+		{
+			alphabetSize = ngram.size();
+			
+		}
+		else
+		{
+			alphabetSize = new NgramAnalyser(inp).getAlphabetSize();
+		}
+	}
 
-    /**
-     * @return int the size of the alphabet of a given input
-     */
-    public int getAlphabetSize() {
-        //TODO replace this line with your code
-        return -1;
-    }
+	/** 
+	 * Analyses the input text for n-grams of size 1.
+	 */
+	public NgramAnalyser(String inp) 
+	{
+		this(1,inp);
+	}
 
-    /**
-     * @return the total number of distinct n-grams appearing
-     *         in the input text.
-     */
-    public int getDistinctNgramCount() {
-        //TODO replace this line with your code
-        return -1;
-    }
+	/**
+	 * @return int the size of the alphabet of a given input
+	 */
+	public int getAlphabetSize() {
+		return alphabetSize;
+	}
 
-    /** 
-     * @return Return a set containing all the distinct n-grams
-     *         in the input string.
-     */
-    public Set<String> getDistinctNgrams() {
-        //TODO replace this line with your code
-        return null;
-    }
+	/**
+	 * @return the total number of distinct n-grams appearing
+	 *         in the input text.
+	 */
+	public int getDistinctNgramCount() {
+		System.out.println(ngram.size());
+		return(ngram.size());
+	}
 
-    /**
-     * @return the total number of n-grams appearing
-     *         in the input text (not requiring them to be distinct)
-     */
-    public int getNgramCount() {
-        //TODO replace this line with your code
-        return -1;
-    }
+	/** 
+	 * @return Return a set containing all the distinct n-grams
+	 *         in the input string.
+	 */
+	public Set<String> getDistinctNgrams() {
+		return(ngram.keySet());
+	}
 
-    /** Return the frequency with which a particular n-gram appears
-     * in the text. If it does not appear at all, return 0.
-     * 
-     * @param ngram The n-gram to get the frequency of
-     * @return The frequency with which the n-gram appears.
-     */
-    public int getNgramFrequency(String ngram) {
-        //TODO replace this line with your code
-        return -1;
-    }
+	/**
+	 * @return the total number of n-grams appearing
+	 *         in the input text (not requiring them to be distinct)
+	 */
+	public int getNgramCount() {
+		return stringSize;
+	}
 
+	/** Return the frequency with which a particular n-gram appears
+	 * in the text. If it does not appear at all, return 0.
+	 * 
+	 * @param ngram The n-gram to get the frequency of
+	 * @return The frequency with which the n-gram appears.
+	 */
+	public int getNgramFrequency(String ngramm) {
+		if (ngram.get(ngramm) == null)
+		{
+			return 0;
+		}
+		else
+		{
+			return ngram.get(ngramm);
+		}
+	}
 
-
-    /**
-     * Generate a summary of the ngrams for this object.
-     * @return a string representation of the n-grams in the input text 
-     * comprising the ngram size and then each ngram and its frequency
-     * where ngrams are presented in alphabetical order.     
-     */
-    public String toString()
-    {
-        //TODO replace this line with your code
-        return null;
-    }
+	/**
+	 * Generate a summary of the ngrams for this object.
+	 * @return a string representation of the n-grams in the input text 
+	 * comprising the ngram size and then each ngram and its frequency
+	 * where ngrams are presented in alphabetical order.     
+	 */
+	public String toString()
+	{
+		double[] keysArray;
+		String asdf = "";
+		keysArray = ngram.keySet().toArray();
+		//TODO replace this line with your code
+		
+		return null;
+	}
 
 }
